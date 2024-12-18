16cde48511000e6a62b16e288122f49f44465a8b
John Paul Ong <jp.ong@smsupermalls.com>
2024-12-18 13:58:05 +0800

    Add nav links

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,16 +1,51 @@
-<nav class="nav"><h1>Products Demo</h1></nav>
+<script lang="ts">
+	const links = [
+		{ id: 'products', href: '/products', label: 'Products' },
+		{ id: 'malls', href: '/malls', label: 'Malls' }
+	];
+</script>
+
+<nav class="nav">
+	<div class="container">
+		<h1>Products Demo</h1>
+		<ul class="menu">
+			{#each links as link (link.id)}
+				<li>
+					<a href={link.href}>{link.label}</a>
+				</li>
+			{/each}
+		</ul>
+	</div>
+</nav>
 
 <style lang="scss">
 	nav.nav {
+		border-bottom: 1px solid var(--c-nav-border);
+		color: var(--c-nav-text);
+	}
+
+	div.container {
+		width: 100%;
+		max-width: 1080px;
 		height: 60px;
 
-		padding: 20px;
+		margin: 0 auto;
 
 		display: flex;
 		align-items: center;
-		justify-content: center;
+		gap: 40px;
+	}
 
-		border-bottom: 1px solid var(--c-nav-border);
-		color: var(--c-nav-text);
+	ul.menu {
+		list-style: none;
+
+		display: flex;
+		flex-flow: row nowrap;
+		gap: 16px;
+		a {
+			font-size: 14px;
+			font-weight: 500;
+			color: inherit;
+		}
 	}
 </style>
